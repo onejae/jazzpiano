244f228539b5ed384ebf9adbc20129ec7ee72002
wonjae lee <onejae@kbs.co.kr>
2023-06-24 20:17:15 +0900

    Render piano

--- src/components/PianoRoll.tsx
+++ src/components/PianoRoll.tsx
@@ -96,79 +96,95 @@ class KeyModel {
   }
 }
 
-const WHITEKEY_WIDTH = 0.2
-const WHITEKEY_HEIGHT = 1
-const BLACKKEY_WIDTH = 0.1
-const BLACKKEY_HEIGHT = 0.7
+const Y_LENGTH_PER_SECOND = 1
+const START_X = -8
+const WHITEKEY_WIDTH = 0.3
+const WHITEKEY_HEIGHT = 1.3
+const BLACKKEY_WIDTH = 0.2
+const BLACKKEY_HEIGHT = 0.9
+const PADDING_X = 0.03
+
+interface KeyRenderSpaceType {
+  x: number
+  y: number
+  z: number
+  w: number
+  h: number
+  d: number
+  name: string
+}
+
+const KeyRenderSpace: { [key: number]: KeyRenderSpaceType } = {}
+const keys = Array(KEY_NUM)
+  .fill('')
+  .map((_, idx) => new KeyModel(START_MIDI_KEY + idx))
+
+for (let lastX = START_X, i = 0; i < keys.length; i++) {
+  const key = keys[i]
+  const [w, h, d] = key.isWhiteKey()
+    ? [WHITEKEY_WIDTH, WHITEKEY_HEIGHT, 0.25]
+    : [BLACKKEY_WIDTH, BLACKKEY_HEIGHT, 0.15]
+
+  const position: Vector3 = key.isWhiteKey()
+    ? [lastX, 0, -0.1]
+    : [lastX - BLACKKEY_WIDTH * 0.8, 0.2, 0.0001]
+
+  KeyRenderSpace[key.midiNumber] = {
+    x: position[0],
+    y: position[1],
+    z: position[2],
+    w: w,
+    h: h,
+    d: d,
+    name: key.noteName,
+  }
+
+  lastX = lastX + (key.isWhiteKey() ? WHITEKEY_WIDTH + PADDING_X : 0)
+}
 
 const VirtualPiano = (props: ThreeElements['mesh']) => {
-  const [keys, setKeys] = useState<KeyModel[]>(
-    Array(KEY_NUM)
-      .fill('')
-      .map((_, idx) => new KeyModel(START_MIDI_KEY + idx))
-  )
-
-  useEffect(() => {
-    keys.forEach((key) => console.log(key))
-  }, [keys])
-
   return (
-    <>
+    <group position={props.position}>
       {keys.map((key: KeyModel, idx) => {
-        const args: [
-          width?: number,
-          height?: number,
-          depth?: number,
-          widthSegments?: number,
-          heightSegments?: number,
-          depthSegments?: number
-        ] = key.isWhiteKey()
-          ? [WHITEKEY_WIDTH, WHITEKEY_HEIGHT, 0.05]
-          : [BLACKKEY_WIDTH, BLACKKEY_HEIGHT, 0.05]
-
-        const position: Vector3 = key.isWhiteKey()
-          ? [-5 + idx * 0.2, 0, 0]
-          : [-5 + idx * 0.1, 0, 1]
+        const renderSpace = KeyRenderSpace[key.midiNumber]
         return (
-          <mesh position={position}>
-            <boxGeometry args={args} />
+          <mesh position={[renderSpace.x, renderSpace.y, renderSpace.z]}>
+            <boxGeometry args={[renderSpace.w, renderSpace.h, renderSpace.d]} />
             <meshStandardMaterial
               color={key.isWhiteKey() ? 'white' : 'black'}
             />
           </mesh>
         )
       })}{' '}
-    </>
+    </group>
   )
 }
 
-const Y_LENGTH_PER_SECOND = 1
-
 const PianoRoll = (props: PianoRollProps) => {
   const ref = useRef<THREE.Group>(null!)
+
   const NoteRender = () => {
     useFrame((state, delta) => {
-      // ref.current.position.y += -3 * delta
+      ref.current.position.y += -3 * delta
     })
+
     return (
-      <>
-        <group ref={ref}>
-          {props.noteEvents.map((note: NoteEvent, idx) => {
-            const startTime = note[NoteEventField.start_s]
-            const pitch = note[NoteEventField.pitch]
-            return (
-              <RollBox
-                position={[
-                  -5 + (pitch - START_MIDI_KEY) * 0.15,
-                  startTime * 2,
-                  0.03,
-                ]}
-                frustumCulled
-              />
-            )
-          })}
-        </group>
-      </>
+      <group ref={ref}>
+        {props.noteEvents.map((note: NoteEvent, idx) => {
+          const startTime = note[NoteEventField.start_s]
+          const pitch = note[NoteEventField.pitch]
+          return (
+            <RollBox
+              position={[
+                -5 + (pitch - START_MIDI_KEY) * 0.15,
+                startTime * 2,
+                0.03,
+              ]}
+              frustumCulled
+            />
+          )
+        })}
+      </group>
     )
   }
 
@@ -178,18 +194,24 @@ const PianoRoll = (props: PianoRollProps) => {
         style={{
           width: '70vw',
           height: 'calc(75vh)',
-          // background: 'red',
+          background: 'grey',
         }}
       >
-        <Canvas>
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <group scale={[1, 1, 1]} rotation={[-0.6, 0, 0]}>
-            {/* <BackBoard position={[0, 0, 0]} /> */}
-            {/* {noteRender} */}
-            <NoteRender />
+        <Canvas
+          camera={{
+            position: [0, 0, 13],
+            fov: 45,
+            near: 0.1,
+            far: 100,
+          }}
+        >
+          <ambientLight position={[10, 0, 0]} intensity={0.3} />
+          <pointLight position={[-3, 0, 300]} intensity={3.3} />
 
-            <VirtualPiano scale={[3, 3, 3]} position={[1, 2, 0]} />
+          <group scale={[1, 1, 1]} rotation={[-0.35, 0, 0]}>
+            {/* <BackBoard position={[0, 0, -0.1]} /> */}
+            <NoteRender />
+            <VirtualPiano scale={[3, 3, 3]} position={[-0.3, -4.2, -2]} />
           </group>
         </Canvas>
       </div>

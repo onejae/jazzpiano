89c6ac304201a8e74e0bd4ac0a41a928e1129380
wonjae lee <onejae@kbs.co.kr>
2023-06-27 15:00:43 +0900

    d:

--- src/components/PianoRoll.tsx
+++ src/components/PianoRoll.tsx
@@ -3,7 +3,6 @@ import { NoteEvent } from 'types/midi'
 
 import { Canvas, ThreeElements, Vector3, useFrame } from '@react-three/fiber'
 
-import { Physics } from '@react-three/rapier'
 import { PitchIndex } from 'constants/notes'
 import React, { Suspense, useEffect, useMemo, useRef, useState } from 'react'
 
@@ -106,7 +105,7 @@ const WHITEKEY_HEIGHT = 1.3
 const BLACKKEY_WIDTH = 0.2
 const BLACKKEY_HEIGHT = 0.9
 const PADDING_X = 0.03
-const BUFFER_SECONDS_FOR_CANDIDATES = 0.5
+const BUFFER_SECONDS_FOR_CANDIDATES = 0.0
 
 interface KeyRenderSpaceType {
   x: number
@@ -148,35 +147,16 @@ for (let lastX = START_X, i = 0; i < keys.length; i++) {
   lastX = lastX + (key.isWhiteKey() ? WHITEKEY_WIDTH + PADDING_X : 0)
 }
 
-// const rayCaster = new Raycaster(new _Vector3(-15, 0, 0), new _Vector3(1, 0, 0))
-
 const VirtualPiano = (props: ThreeElements['mesh']) => {
-  // const [ray, setRay] = useState(null)
-
-  // useEffect(() => {
-  //   if (!ray) return
-  //   const intersect = ray.intersectObject(box)
-  //   console.log(intersect)
-  // }, [ray])
-
   return (
     <group position={props.position}>
       <mesh position={[0, WHITEKEY_HEIGHT * 0.5, 0]}>
         {/* <boxGeometry args={[20, 0.0, 1]} /> */}
       </mesh>
-      {/* <raycaster
-        ref={setRay}
-        // ray={{
-        //   origin: new _Vector3(-3, 0, 0),
-        //   direction: new _Vector3(1, 0, 0),
-        // }}
-        ray={new Ray(new _Vector3(-3, 0, 0), new _Vector3(1, 0, 0))}
-      ></raycaster> */}
       {keys.map((key: KeyModel, idx) => {
         const renderSpace = KeyRenderSpace[key.midiNumber]
         return (
           <mesh
-            raycast={(a, b) => {}}
             position={[renderSpace.x, renderSpace.y, renderSpace.z]}
             key={idx}
           >
@@ -191,12 +171,22 @@ const VirtualPiano = (props: ThreeElements['mesh']) => {
   )
 }
 
+interface RenderInfo {
+  timer: number
+  indexFrom: number
+  blockRail: { [key: number]: NoteEvent[] }
+}
+
 const PianoRoll = (props: PianoRollProps) => {
   const ref = useRef<THREE.group>(null!)
-  const [currentTimeSeconds, setCurrentTimeSeconds] = useState<number>(0)
   const refNoteBlocks = Array.from({ length: 10000 }, () =>
     useRef<THREE.mesh>(null!)
   )
+  const renderInfo = useRef<RenderInfo>({
+    timer: 0,
+    indexFrom: 0,
+    blockRail: {},
+  })
   const [checkFromIndex, setCheckFromIndex] = useState(0)
 
   const noteBlocks = useMemo(() => {
@@ -205,11 +195,12 @@ const PianoRoll = (props: PianoRollProps) => {
       const pitch = note[2]
       const duration = note[1] - note[0]
       const renderSpace = KeyRenderSpace[pitch]
-      return (
+
+      const renderObject = (
         <mesh
           position={[
             renderSpace.x,
-            5 + startTime * Y_LENGTH_PER_SECOND,
+            startTime * Y_LENGTH_PER_SECOND,
             renderSpace.z,
           ]}
           frustumCulled
@@ -225,50 +216,36 @@ const PianoRoll = (props: PianoRollProps) => {
           <meshStandardMaterial color={renderSpace.color} />
         </mesh>
       )
+
+      return renderObject
     })
   }, [props.noteEvents, refNoteBlocks])
 
+  useEffect(() => {
+    props.noteEvents.forEach((note: NoteEvent, idx) => {
+      const pitch = note[2]
+      if (renderInfo.current) {
+        renderInfo.current.blockRail[pitch] =
+          renderInfo.current.blockRail[pitch] || []
+        renderInfo.current.blockRail[pitch].push(note)
+      }
+    })
+  }, [props.noteEvents])
+
   const NoteRender = () => {
     useFrame((state, delta) => {
-      ref.current.translateY(-3 * delta)
+      if (!renderInfo.current) return
+      ref.current.translateY(delta * -Y_LENGTH_PER_SECOND)
 
-      // get check candidates
-      const candidates: THREE.mesh[] = []
-
-      for (let i = checkFromIndex; i <= props.noteEvents.length; i++) {
+      for (let i = checkFromIndex; i < props.noteEvents.length; i++) {
         const noteEvent = props.noteEvents[i]
 
-        if (
-          noteEvent[0] >= currentTimeSeconds + BUFFER_SECONDS_FOR_CANDIDATES &&
-          noteEvent[1] + BUFFER_SECONDS_FOR_CANDIDATES <= currentTimeSeconds
-        ) {
-          candidates.push(refNoteBlocks[i])
+        if (noteEvent[0] <= renderInfo.current.timer) {
+          refNoteBlocks[i].current.material.color.set(0xff0000)
         }
       }
-      //
 
-      // if (refNoteBlocks[1].current) {
-      //   const vector = new THREE.Vector3()
-      //   refNoteBlocks[1].current.getWorldPosition(vector)
-
-      //   console.log(vector)
-      // }
-
-      // const test = refNoteBlocks.map((ref) => {
-      // return ref.current
-      // })
-
-      // if (refNoteBlocks.length && refNoteBlocks[0].current) {
-      //   const rayCaster = new Raycaster()
-      //   rayCaster.set(new _Vector3(-50, -3, -2), new _Vector3(1, 0, 0))
-      //   const a = rayCaster.intersectObjects(test)
-
-      //   if (a.length) {
-      //     a.forEach((block) => {
-      //       block.object.material.color.set(0xff0000)
-      //     })
-      //   }
-      // }
+      renderInfo.current.timer += delta
     })
 
     return <group ref={ref}>{noteBlocks}</group>
@@ -281,16 +258,10 @@ const PianoRoll = (props: PianoRollProps) => {
 
   const ref2 = useRef<THREE.group>(null!)
 
-  useEffect(() => {
-    console.log('------------------')
-
-    const vector = new THREE.Vector3()
-
-    if (ref2.current) {
-      ref2.current.getWorldPosition(vector)
-      console.log(vector)
-    }
-  }, [ref2.current])
+  const rayVector = new _Vector3(-50, -3, 0)
+  // rayVector.applyAxisAngle(new _Vector3(1, 0, 0), -0.3)
+  const ray = new Ray(rayVector, new _Vector3(1, 0, 0))
+  const rayRef = useRef<THREE.Raycaster>(null!)
 
   return (
     <div style={{ width: '100%', justifyContent: 'center', display: 'flex' }}>
@@ -310,20 +281,19 @@ const PianoRoll = (props: PianoRollProps) => {
           }}
         >
           <Suspense>
-            <Physics gravity={[0, 0, 0]} interpolate={false} colliders={false}>
-              <ambientLight position={[10, 0, 0]} intensity={0.3} />
-              <pointLight position={[-3, 0, 300]} intensity={3.3} />
+            <ambientLight position={[10, 0, 0]} intensity={0.3} />
+            <pointLight position={[-3, 0, 300]} intensity={3.3} />
 
-              <group
-                scale={[1, 1, 1]}
-                rotation={[-0.0, 0, 0]}
-                position={[0, -4.5, 0]}
-                ref={ref2}
-              >
-                <NoteRender />
-                <VirtualPiano />
-              </group>
-            </Physics>
+            <group
+              scale={[1, 1, 1]}
+              rotation={[-0.3, 0, 0]}
+              position={[0, -4.5, 0]}
+              ref={ref2}
+            >
+              <raycaster ray={ray} ref={rayRef} />
+              <NoteRender />
+              <VirtualPiano />
+            </group>
           </Suspense>
         </Canvas>
       </div>
